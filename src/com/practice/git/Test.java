11a3476cc61609eabeed354024d52925c74bb6a3
Ashok Nampelli <jalaja.200984@gmail.com>
2022-07-20 16:06:29 -0700

    Modified

--- src/com/practice/git/Test.java
+++ src/com/practice/git/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public void addData()
 	{
 		System.out.println(" Addedd git...");
+		System.out.println("modified");
 	}
 	public static void main(String[] args) {
 		System.out.println("main method");
