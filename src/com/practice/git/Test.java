a42496ee2862ac2a3f00c7767ef8cf982e45ac3b
Ashok Nampelli <jalaja.200984@gmail.com>
2022-07-20 15:40:20 -0700

    Added git-demo project tot he local repository

---
+++ src/com/practice/git/Test.java
@@ -0,0 +1,14 @@
+package com.practice.git;
+
+public class Test {
+	public void addData()
+	{
+		System.out.println(" Addedd git...");
+	}
+	public static void main(String[] args) {
+		System.out.println("main method");
+		Test t = new Test();
+		t.addData();
+	}
+
+}
